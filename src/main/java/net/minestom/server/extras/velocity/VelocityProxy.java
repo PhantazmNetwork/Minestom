fa41f796089c88a1dff937632337699a3815b76a
tahmid-23 <60953955+tahmid-23@users.noreply.github.com>
2022-07-15 16:48:17 -0400

    move texture reading

--- src/main/java/net/minestom/server/extras/velocity/VelocityProxy.java
+++ src/main/java/net/minestom/server/extras/velocity/VelocityProxy.java
@@ -105,16 +105,22 @@ public final class VelocityProxy {
         final int properties = reader.readVarInt();
         for (int i = 0; i < properties; i++) {
             final String name = reader.readSizedString(Short.MAX_VALUE);
-            final String value = reader.readSizedString(Short.MAX_VALUE);
-            final String signature = reader.readBoolean() ? reader.readSizedString(Short.MAX_VALUE) : null;
 
             switch (name) {
                 case "textures" -> {
+                    final String value = reader.readSizedString(Short.MAX_VALUE);
+                    final String signature = reader.readBoolean() ? reader.readSizedString(Short.MAX_VALUE) : null;
+
                     if (value != null && signature != null) {
                         playerSkin = new PlayerSkin(value, signature);
                     }
                 }
                 case "protocolVersion" -> {
+                    final String value = reader.readSizedString(Short.MAX_VALUE);
+                    if (reader.readBoolean()) {
+                        reader.readSizedString(String.valueOf(Integer.MIN_VALUE).length());
+                    }
+
                     if (value != null) {
                         try {
                             protocolVersion = Integer.parseInt(value);
