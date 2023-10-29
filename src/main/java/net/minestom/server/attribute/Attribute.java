5801a6f2185523a0c1315670b963044d1406b0f2
steank <dnd.initiative.tracker@gmail.com>
2023-10-29 16:11:50 -0700

    delete unused maxValue field, update unit tests

--- src/main/java/net/minestom/server/attribute/Attribute.java
+++ src/main/java/net/minestom/server/attribute/Attribute.java
@@ -10,28 +10,22 @@ import java.util.concurrent.ConcurrentHashMap;
 /**
  * Represents a {@link net.minestom.server.entity.LivingEntity living entity} attribute.
  */
-public record Attribute(String key, float defaultValue, float maxValue, boolean sendToClient) {
+public record Attribute(String key, float defaultValue, boolean sendToClient) {
     private static final Map<String, Attribute> ATTRIBUTES = new ConcurrentHashMap<>();
 
-    public static final Attribute MAX_HEALTH = (new Attribute("generic.max_health", 20, 1024, true)).register();
-    public static final Attribute FOLLOW_RANGE = (new Attribute("generic.follow_range", 32, 2048, true)).register();
-    public static final Attribute KNOCKBACK_RESISTANCE = (new Attribute("generic.knockback_resistance", 0, 1, true)).register();
-    public static final Attribute MOVEMENT_SPEED = (new Attribute("generic.movement_speed", 0.25f, 1024, true)).register();
-    public static final Attribute ATTACK_DAMAGE = (new Attribute("generic.attack_damage", 2, 2048, true)).register();
-    public static final Attribute ATTACK_SPEED = (new Attribute("generic.attack_speed", 4, 1024, true)).register();
-    public static final Attribute FLYING_SPEED = (new Attribute("generic.flying_speed", 0.4f, 1024, true)).register();
-    public static final Attribute ARMOR = (new Attribute("generic.armor", 0, 30, true)).register();
-    public static final Attribute ARMOR_TOUGHNESS = (new Attribute("generic.armor_toughness", 0, 20, true)).register();
-    public static final Attribute ATTACK_KNOCKBACK = (new Attribute("generic.attack_knockback", 0, 5, true)).register();
-    public static final Attribute LUCK = (new Attribute("generic.luck", 0, 1024, true)).register();
-    public static final Attribute HORSE_JUMP_STRENGTH = (new Attribute("horse.jump_strength", 0.7f, 2, true)).register();
-    public static final Attribute ZOMBIE_SPAWN_REINFORCEMENTS = (new Attribute("zombie.spawn_reinforcements", 0, 1, true)).register();
-
-    public Attribute {
-        if (defaultValue > maxValue) {
-            throw new IllegalArgumentException("Default value cannot be greater than the maximum allowed");
-        }
-    }
+    public static final Attribute MAX_HEALTH = (new Attribute("generic.max_health", 20, true)).register();
+    public static final Attribute FOLLOW_RANGE = (new Attribute("generic.follow_range", 32, true)).register();
+    public static final Attribute KNOCKBACK_RESISTANCE = (new Attribute("generic.knockback_resistance", 0, true)).register();
+    public static final Attribute MOVEMENT_SPEED = (new Attribute("generic.movement_speed", 0.25f, true)).register();
+    public static final Attribute ATTACK_DAMAGE = (new Attribute("generic.attack_damage", 2, true)).register();
+    public static final Attribute ATTACK_SPEED = (new Attribute("generic.attack_speed", 4, true)).register();
+    public static final Attribute FLYING_SPEED = (new Attribute("generic.flying_speed", 0.4f, true)).register();
+    public static final Attribute ARMOR = (new Attribute("generic.armor", 0, true)).register();
+    public static final Attribute ARMOR_TOUGHNESS = (new Attribute("generic.armor_toughness", 0, true)).register();
+    public static final Attribute ATTACK_KNOCKBACK = (new Attribute("generic.attack_knockback", 0, true)).register();
+    public static final Attribute LUCK = (new Attribute("generic.luck", 0, true)).register();
+    public static final Attribute HORSE_JUMP_STRENGTH = (new Attribute("horse.jump_strength", 0.7f, true)).register();
+    public static final Attribute ZOMBIE_SPAWN_REINFORCEMENTS = (new Attribute("zombie.spawn_reinforcements", 0, true)).register();
 
     /**
      * Register this attribute.
