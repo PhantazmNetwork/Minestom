a6df51ee480c49054b5b4db1297205b4c0a651f0
steank <dnd.initiative.tracker@gmail.com>
2023-12-28 07:23:41 -0800

    update gradle, do a bunch of things to optimize build time

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,4 @@
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
-enableFeaturePreview("VERSION_CATALOGS")
 
 dependencyResolutionManagement {
     repositories {
@@ -19,5 +18,4 @@ pluginManagement {
 rootProject.name = "Minestom"
 include("code-generators")
 include("jmh-benchmarks")
-include("jcstress-tests")
 include("testing")
