31e82c88c7c2d8de0f642823f39a50ad9d299085
steank <dnd.initiative.tracker@gmail.com>
2024-03-02 07:46:45 -0800

    Fix encoding issue with Minestom
     * Specify UTF-8 encoding for the JavaCompile task through common-conventions.gradle.kts

--- build-logic/src/main/kotlin/minestom.common-conventions.gradle.kts
+++ build-logic/src/main/kotlin/minestom.common-conventions.gradle.kts
@@ -16,6 +16,8 @@ java {
 
 tasks {
     withType<JavaCompile> {
+        options.encoding = "UTF-8"
+
         // We are fully aware, that we should be suppressing these instead of ignoring them here, but man keep my terminal clean.
         options.compilerArgs.addAll(listOf("-Xlint:none", "-Xlint:-deprecation", "-Xlint:-unchecked"))
     }
